16ffc2eae322b9e68756ef18fa96d4cf56c6b3d4
Richard Samuel <98638849+samuelard7@users.noreply.github.com>
2025-09-22 18:47:20 +0530

    49688

---
+++ orders/utils.py
@@ -0,0 +1,22 @@
+import string
+import secrets
+from django.db import IntegrityError
+from .models import Coupon
+
+def generate)coupon_code(length=10):
+    if length < 1:
+        raise ValueError("Coupon code length miust be at least 1")
+    characters = string.ascii_uppercase + string.digits
+
+    max_attempts = 100
+
+    for _ in range(max_attempts):
+        code = ''.join(secrets.choice(characters) for _ in range(length))
+
+        try:
+            if not Coupon.objects.filter(code=code).exists():
+                return code
+        except Exception as e:
+            raise RuntimeError(f"Error checking coupon code uniqueness: {str(e)}")
+    raise RuntimeError("Unable to generate a unique coupon code after maximum attempts")
+    
