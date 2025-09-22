82743dc28c5190213b65ea8af1aabc19eb9210e1
samuelard7 <samuelrichard214@gmail.com>
2025-09-22 18:55:58 +0530

    migrated

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Coupon(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    discount = models.DecimalField(max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.code
