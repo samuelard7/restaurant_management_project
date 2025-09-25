ceba87a080e4212818a8da07576d326040eb571c
Richard Samuel <98638849+samuelard7@users.noreply.github.com>
2025-09-26 01:06:34 +0530

    50438

--- orders/urls.py
+++ orders/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
+from .views import MenuItemSearchViewSet, OrderHistoryView
 from rest_framework.routers import DefaultRouter
-from .views import MenuItemSearchViewSet
 
-# Create a router and register the viewset
 router = DefaultRouter()
 router.register(r'menu-items/search', MenuItemSearchViewSet, basename='menu-item-search')
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('api/order-history/', OrderHistoryView.as_view(), name='order_history'),
 ]
