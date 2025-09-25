c6987bfbddd612645bf59791d0cb9ca45d23ce80
Richard Samuel <98638849+samuelard7@users.noreply.github.com>
2025-09-26 00:58:54 +0530

    50438

--- orders/serializers.py
+++ orders/serializers.py
@@ -8,18 +8,11 @@ class MenuItemSerializer(serializers.ModelSerializer):
         model = MenuItem
         fields = ['id', 'name', 'description', 'price', 'category_name']
 
-
 class OrderItemSerializer(serializers.ModelSerializer):
-    menu_item_name = serializers.CharField(source='menu_item.name', read_only=True)
-
-    class Meta:
-        model = OrderItem
-        fields = ['id', 'menu_item_name', 'quantity', 'price']
-
-class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True, read_only=True)
     status_name = serializers.CharField(source='status.name', read_only=True)
 
     class Meta:
         model = Order
-        fields = ['id', 'created_at', 'total_amount', 'status_name', 'items']
+        fields = ['id','created_at','total_amount','status_name','items']
+        
