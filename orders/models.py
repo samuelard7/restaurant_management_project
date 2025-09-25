9cae9849d2cc4be9631d19b47c46d048969b3d04
samuelard7 <samuelrichard214@gmail.com>
2025-09-26 01:29:31 +0530

    added utils

--- orders/models.py
+++ orders/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
-from .models import OrderStatus, MenuItem
 from home.models import MenuCategory
 
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(MenuCategory, on_delete=models.SET_NULL, 
+    null=True, related_name='menu_items')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = "Menu Item"
+        verbose_name_plural = "Menu Items"
 
 class Coupon(models.Model):
     code = models.CharField(max_length=20, unique=True)
@@ -48,19 +60,6 @@ class OrderItem(models.Model):
         verbose_name= "Order Item"
         verbose_name_plural = "Order Items"
 
-class MenuItem(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey(MenuCategory, on_delete=models.SET_NULL, 
-    null=True, related_name='menu_items')
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
-    class Meta:
-        verbose_name = "Menu Item"
-        verbose_name_plural = "Menu Items"
 
 PENDING = 'Pending'
 PROCESSING = 'Processing'
--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'account',
     'products',
     'orders',
+    'utils',
 ]
 
 MIDDLEWARE = [
