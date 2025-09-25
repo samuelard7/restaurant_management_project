b93da9e0e82df0a7819b3207af95360a917978fc
Richard Samuel <98638849+samuelard7@users.noreply.github.com>
2025-09-26 01:11:02 +0530

    50437

--- orders/models.py
+++ orders/models.py
@@ -52,7 +52,8 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey(MenuCategory, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(MenuCategory, on_delete=models.SET_NULL, 
+    null=True, related_name='menu_items')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
